c235bd4cc4d900fc98744f2521f7f201c20f8c6f
ferdinand-hub22 <77827964+ferdinand-hub22@users.noreply.github.com>
2023-04-13 17:03:52 +0200

    Update editage.js

--- frontend/editage.js
+++ frontend/editage.js
@@ -10,6 +10,44 @@ window.addEventListener('load', () => {
 const blockForm = document.querySelector('#block-form');
 blockForm.addEventListener('submit', (event) => {
 	event.preventDefault();
-	addBlock();
 });
 
+const ajout = () => {
+	// Obtener el formulario y el contenedor de bloques
+const blockForm = document.getElementById('block-form');
+const blockContainer = document.createElement('div');
+blockContainer.id = 'block-container';
+document.querySelector('main').appendChild(blockContainer);
+const numVariables = document.getElementById('block-variables').value;
+const blockList = document.createElement('ul');
+
+
+// Agregar un nuevo bloque al formulario
+blockForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const blockName = document.getElementById('block-name').value;
+
+// Crear el nuevo bloque y agregarlo al contenedor
+const newBlock = document.createElement('div');
+newBlock.classList.add('block');
+const blockTitle = document.createElement('h3');
+blockTitle.textContent = blockName;
+newBlock.appendChild(blockTitle);
+newBlock.appendChild(blockList);
+blockContainer.appendChild(newBlock);
+});
+
+// Crear un campo de texto para cada variable
+console.log("here" + numVariables);
+for (let i = 0; i < numVariables; i++) {
+	console.log("in here");
+  const listItem = document.createElement('li');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  input.type = 'text';
+  listItem.appendChild(label);
+  listItem.appendChild(input);
+  blockList.appendChild(listItem);
+}
+
+}
